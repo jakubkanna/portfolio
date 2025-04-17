b4b7c6ecbaa3c1605a7a13c06513a57e9945240c
jakubkanna <hello.jakubkanna@gmail.com>
2025-04-17 15:32:45 +0200

    feat: add cv form

--- src/components/sections/CV.section.tsx
+++ src/components/sections/CV.section.tsx
@@ -12,6 +12,7 @@ export default function CVSection({
   threshold,
 }: SectionProps) {
   const [content, setContent] = useState<string>("");
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     // Dynamically import the more.md file
@@ -28,6 +29,10 @@ export default function CVSection({
     loadFile();
   }, []);
 
+  const triggerForm = () => {
+    setIsVisible(true);
+  };
+
   return (
     <Section
       id="CV"
@@ -38,7 +43,61 @@ export default function CVSection({
     >
       <div className="container">
         {isMobile() ? (
-          <Button>Send the CV directly to your inbox</Button>
+          <>
+            {!isVisible ? (
+              <>
+                <Button onClick={triggerForm}>
+                  Send the CV directly to your email
+                </Button>
+              </>
+            ) : (
+              <div id="mc_embed_signup">
+                <form
+                  action="https://jakubkanna.us20.list-manage.com/subscribe/post?u=837187f00032092e6c9a48147&amp;id=7ab2644c97&amp;f_id=009a5eeef0"
+                  method="post"
+                  target="_blank"
+                  noValidate
+                >
+                  <div>
+                    <input
+                      type="email"
+                      name="EMAIL"
+                      placeholder="your-email@example.com*"
+                      required
+                      id="mce-EMAIL"
+                      className="required email"
+                    />{" "}
+                  </div>
+                  <input type="hidden" name="tags" value="4227759" />
+                  <div
+                    style={{ position: "absolute", left: "-5000px" }}
+                    aria-hidden="true"
+                  >
+                    <input
+                      type="text"
+                      name="b_837187f00032092e6c9a48147_7ab2644c97"
+                      tabIndex={-1}
+                      defaultValue=""
+                    />
+                  </div>
+                  <div className="clear foot">
+                    <input
+                      type="submit"
+                      name="subscribe"
+                      id="mc-embedded-subscribe"
+                      className="btn"
+                      value="Send"
+                    />
+                  </div>
+                </form>
+                <small>
+                  * Your email will be used to send the CV via Mailchimp and
+                  stored securely for contact purposes. You can unsubscribe
+                  anytime.
+                </small>
+              </div>
+            )}
+          </>
         ) : (
           <Markdown
             remarkPlugins={[remarkGfm]}
