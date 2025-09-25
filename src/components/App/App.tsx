cd35a1b8a49711e4c113365187baa1691e0a03b8
jakubkanna <hello.jakubkanna@gmail.com>
2025-09-25 11:18:45 +0200

    refactor: clean up dark mode related code and comments

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,39 +3,40 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import Sections from "../sections/Sections";
 import Menu from "../Menu/Menu";
-import { applyDarkMode, setDarkMode } from "../../utils/toggleDarkMode";
+import { setDarkMode } from "../../utils/toggleDarkMode";
 import ThreeButton from "../Buttons/ThreeButton";
 import isMobile from "is-mobile";
 
 function App() {
   useEffect(() => {
     document.title = `JAKUB KANNA — Graphic Designer, Full-stack Developer, Artist`;
+    setDarkMode();
   }, []);
 
   const [menuHidden, setMenuHidden] = useState(isMobile());
 
   const toggleMenu = () => setMenuHidden((prev) => !prev);
 
-  useEffect(() => {
-    // Apply dark mode based on localStorage setting
-    const darkModeSetting = localStorage.getItem("dark_mode");
+  // useEffect(() => {
+  //   // Apply dark mode based on localStorage setting
+  //   const darkModeSetting = localStorage.getItem("dark_mode");
 
-    // default to dark mode
-    if (!darkModeSetting) setDarkMode();
+  //   // default to dark mode
+  //   if (!darkModeSetting) setDarkMode();
 
-    //
-    applyDarkMode(darkModeSetting === "true");
+  //   //
+  //   applyDarkMode(darkModeSetting === "true");
 
-    // Optional: Listen to changes in localStorage (if multiple tabs)
-    const handleStorageChange = (e: StorageEvent) => {
-      if (e.key === "dark_mode") {
-        applyDarkMode(e.newValue === "true");
-      }
-    };
+  //   // Optional: Listen to changes in localStorage (if multiple tabs)
+  //   const handleStorageChange = (e: StorageEvent) => {
+  //     if (e.key === "dark_mode") {
+  //       applyDarkMode(e.newValue === "true");
+  //     }
+  //   };
 
-    window.addEventListener("storage", handleStorageChange);
-    return () => window.removeEventListener("storage", handleStorageChange);
-  }, []);
+  //   window.addEventListener("storage", handleStorageChange);
+  //   return () => window.removeEventListener("storage", handleStorageChange);
+  // }, []);
 
   return (
     <main>
